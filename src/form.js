51f76ce5551af1ec83be8c28870bced2df7361ef
Matheus Motta <matheusdomingues423@gmail.com>
2023-08-11 15:34:22 -0300

    add javascript

--- src/form.js
+++ src/form.js
@@ -2,4 +2,14 @@ const form = document.querySelector('#form')
 
 form.addEventListener('submit', (event) => {
   event.preventDefault()
+
+  try {
+    loading()
+
+    const formData = new FormData(form)
+    const inputUrl = formData.get('url')
+    
+  } catch (error) {
+    console.log('[SUBMIT_ERROR]:', error)
+  }
 })
---
+++ src/loader.js
@@ -0,0 +1,7 @@
+export function loading(){
+  document.documentElement.classList.add = '.loading'
+}
+
+export function stopLoading(){
+  document.documentElement.classList.remove = '.loading'
+}
